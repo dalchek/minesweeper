cc8c3bf80139f15dd9a3f8322a74ad27adadc958
dalchek <dalchek999@gmail.com>
2024-04-15 10:59:50 +0200

    show all bombs if one is clicked and  game over

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded",() => {
     const grid = document.querySelector('.grid',)
     const flagsLeft = document.querySelector('#flags-left');
+    const result = document.querySelector('#result')
     const width = 10
     let bombAmount = 20
     let squares = []
+    let isGameOver = false
 
 
     function createBoard(){
@@ -34,11 +36,46 @@ document.addEventListener("DOMContentLoaded",() => {
             })
         }
         // add numbers
-        
+        for (let i = 0; i < squares.length; i++) {
+            let total = 0
+            const isLeftEdge = (i % width === 0)
+            const isRightEdge = (i % width === width - 1)
+
+            if (squares[i].classList.contains('valid')) {
+                if (i > 0 && !isLeftEdge && squares[i - 1].classList.contains('bomb')) total++
+                if (i > 9 && !isRightEdge && squares[i + 1].classList.contains('bomb')) total++
+                if (i > 10 && !isRightEdge && squares[i - width].classList.contains('bomb')) total++
+                if (i > 11 && !isLeftEdge && squares[i - width - 1].classList.contains('bomb')) total++
+                if (i < 99 && !isRightEdge && squares[i + 1].classList.contains('bomb')) total++
+                if (i < 90 && !isLeftEdge && squares[i - 1 - width].classList.contains('bomb')) total++
+                if (i < 88 && !isRightEdge && squares[i + 1 + width].classList.contains('bomb')) total ++
+                if (i < 89 && squares[i + width].classList.contains('bomb')) total ++
+                squares[i].setAttribute('data', total)
+            }
+        }
     }
     createBoard()
 
     function click(square) {
         console.log(square);
+        if (isGameOver || square.classList.contains('checked') || square.classList.contains('flagged')) return
+
+        if (square.classList.contains('bomb')) {
+            gameOver()
+        }
+    }
+
+    function gameOver() {
+        result.innerHTML = 'BOOM! Game Over'
+        isGameOver = true
+
+        // show all the bombs
+        squares.forEach(function(square) {
+            if (square.classList.contains('bomb')) {
+                square.innerHTML = '💣'
+                square.classList.remove('bomb')
+                square.classList.add('checked')
+            }
+        })
     }
 })
